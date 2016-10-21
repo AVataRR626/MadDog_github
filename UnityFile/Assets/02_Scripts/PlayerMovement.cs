3ac8242edec46add60e32dfd3592db48366224a6
Hayden <thatrandomguy80@gmail.com>
2016-10-22 02:27:40 +1100

    Player Movement progress Trampoline added to scene1

--- UnityFile/Assets/02_Scripts/PlayerMovement.cs
+++ UnityFile/Assets/02_Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using System.Collections;
 
 
 public class PlayerMovement : MonoBehaviour {
+
 	public float poopAnimTime = 1f;
 
-	[SerializeField]
-	private float rotSpeed = 100f;
-	[SerializeField]
-	private float moveSpeed = 5f;
-	[SerializeField]
-	private float jumpPower = 5f;
+	//if this is the current active movement script
+	//for use with ride-on etc - only one should be acttive
+	private bool isActiveMovement;
 
+	//movement vars
+	private float rotSpeed = 150f;
+	private float moveSpeed = 8f;
+	private float jumpPower = 300f;
+	//for animations
 	private bool freeze = false;
 	private float lastTime = -5f;
 	// Use this for initialization
@@ -27,21 +30,42 @@ public class PlayerMovement : MonoBehaviour {
 		}
 		
 		float rVector =0f,mVector=0f;
-		if (Input.GetButton("D")) rVector +=1;
-		if (Input.GetButton("A")) rVector -=1;
+		if (Input.GetButton("S")) {//reverses left right while moving backwards
+			mVector -=1;
+			if (Input.GetButton("D")) rVector -=1;
+			if (Input.GetButton("A")) rVector +=1;
+		}else{
+			if (Input.GetButton("D")) rVector +=1;
+			if (Input.GetButton("A")) rVector -=1;
+		}
 		if (Input.GetButton("W")) mVector +=1;
-		if (Input.GetButton("S")) mVector -=1;
 		
 		
-		transform.Rotate(new Vector3(0,rVector*rotSpeed*Time.deltaTime,0));
+		transform.Rotate(new Vector3(0, rVector * rotSpeed * Time.deltaTime,0));
 
-		if((!Physics.Raycast(transform.position, transform.forward,0.5f) || mVector<0 ) && !freeze)
-			transform.Translate((transform.forward) * moveSpeed * Time.deltaTime * mVector,Space.World);
+		Ray ray = new Ray(transform.position,transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 0.5f)) {
+			if (!hit.transform.gameObject.isStatic || mVector < 0)// if not near wall or backing up
+				move(mVector);
+		}else
+			move(mVector);//normal movement
+
+
 		if ((Input.GetButtonDown("Jump") && Time.time - lastTime > 1f) && !freeze){
+			Debug.Log("jump");
 			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
 			lastTime = Time.time;
 		}
 	}
+
+
+	private void move(float mVector){
+	if(!freeze)
+		transform.Translate((transform.forward) * moveSpeed * Time.deltaTime * mVector,Space.World);
+	}
+
 
 	public void Freeze(){
 		Debug.Log("Freeze");
