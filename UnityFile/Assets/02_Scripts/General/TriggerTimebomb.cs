023e36f1b2a36fdd1c133b67eacb07bbdaa746b8
AVataRR626 <matt.cabanag@gmail.com>
2016-10-22 04:03:45 +1100

    pickuppable poop
    once the person enters the poop's trigger area, a timebomb timer gets
    triggered...

---
+++ UnityFile/Assets/02_Scripts/General/TriggerTimebomb.cs
@@ -0,0 +1,35 @@
+﻿/*
+NSI GameJam 11
+Mad Dog
+
+Gets tripped when anything enters its trigger range;
+-Matt Cabanag
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class TriggerTimebomb : MonoBehaviour
+{
+
+    public float timer = 3;
+    public string[] tagList;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (Radar.TagInList(col.tag, ref tagList))
+            Destroy(transform.root.gameObject, timer);
+    }
+}
