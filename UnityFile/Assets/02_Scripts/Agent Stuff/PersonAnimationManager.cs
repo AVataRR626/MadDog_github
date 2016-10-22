15597525f7ae788d2cc98b177c3cea0d0039c4ae
AVataRR626 <matt.cabanag@gmail.com>
2016-10-22 15:52:38 +1100

    person animations
    -animations now linked to agent actions
    -person prefabs created

--- UnityFile/Assets/02_Scripts/Agent Stuff/PersonAnimationManager.cs
+++ UnityFile/Assets/02_Scripts/Agent Stuff/PersonAnimationManager.cs
@@ -1,20 +1,72 @@
-﻿using UnityEngine;
+﻿/*
+NSI GameJam 11
+Mad Dog
+
+Person animation manager
+-Matt Cabanag
+*/
+using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(Animator))]
 public class PersonAnimationManager : MonoBehaviour
 {
+    [Header("Anim Parameters")]
+    public float walkSpeedThreshold = 0.5f;
+    public string runningStateString = "Running";
+    public string actionStateString = "Cleaning";
 
-    Animation anim;
+    [Header("Component Links")]
+    public Chase chaser;
+    public DestinationMover mover;
+    public Patrol patroller;
+
+    Animator anim;
 
 	// Use this for initialization
-	void Start () {
-        anim = GetComponent<Animation>();
-	}
+	void Start ()
+    {
+        if(chaser == null)
+            chaser = transform.root.GetComponent<Chase>();
+
+        if(mover == null)
+            mover = transform.root.GetComponent<DestinationMover>();
+
+        if (patroller == null)
+            patroller = transform.root.GetComponent<Patrol>();
+
+
+        anim = GetComponent<Animator>();
+        anim.SetBool(runningStateString, false);
+        anim.SetBool(actionStateString, false);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        ManageAnimations();
+    }
+
+    void ManageAnimations()
+    {
+        if (mover.agent.velocity.magnitude > walkSpeedThreshold)
+        {
+            anim.SetBool(runningStateString, true);
+            anim.SetBool(actionStateString, false);
+        }
+        else
+        {
+            anim.SetBool(runningStateString, false);
+
+            if (chaser.InChaseRange)
+            {
+                anim.SetBool(actionStateString, false);
+            }
+            else
+            {
+                if (chaser.Dist2Target <= chaser.minChaseRange)
+                    anim.SetBool(actionStateString, true);
+            }
+        }
+    }
 }
