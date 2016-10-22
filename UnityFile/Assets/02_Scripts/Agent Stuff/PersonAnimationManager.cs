2569fdb42a342ce01e73b78de1812c568c8af525
AVataRR626 <matt.cabanag@gmail.com>
2016-10-22 15:15:57 +1100

    tinted knockovers
    items already knocked over will be tinted to indicate they are no longer
    knockable

---
+++ UnityFile/Assets/02_Scripts/Agent Stuff/PersonAnimationManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animation))]
+public class PersonAnimationManager : MonoBehaviour
+{
+
+    Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
