d6bada2a2a20dc108f7b4a2e2d246d6d3348490a
AVataRR626 <matt.cabanag@gmail.com>
2016-10-23 11:41:25 +1100

    fixed stupid people bug
    gleh

---
+++ UnityFile/Assets/02_Scripts/UI/RendererDisabler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RendererDisabler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Renderer>().enabled = false;
+	}
+
+}
