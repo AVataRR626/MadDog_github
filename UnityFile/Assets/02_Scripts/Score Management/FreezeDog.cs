45143082be76dce653a0f2a4c08bd779617c2325
AVataRR626 <matt.cabanag@gmail.com>
2016-10-23 14:55:16 +1100

    lose on end time
    -opens game over screen and freezes dog at end of time

---
+++ UnityFile/Assets/02_Scripts/Score Management/FreezeDog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeDog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<PlayerMovement>().Speed = 0;
+    }
+
+}
